47c6530b6cfa253920deeb3c56737c74b8f066bd
vanishreearsu <124886581+vanishreearsu@users.noreply.github.com>
2023-03-20 20:54:51 +0530

    Add files via upload

---
+++ using of point.py
@@ -0,0 +1,12 @@
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def sqSum(self):
+       return self.x ** 2 + self.y ** 2 + self.z ** 2
+
+
+p = Point(1, 3, 5)
+print(p.sqSum()) 
